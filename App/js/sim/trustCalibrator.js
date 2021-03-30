259813146b232f78625cd71b095480d9816c391f
Joakim Sigvald <joakim.sigvald@gmail.com>
2021-03-30 20:57:48 +0200

    test covered trustCalibrator and improved algorithm

--- App/js/sim/trustCalibrator.js
+++ App/js/sim/trustCalibrator.js
@@ -2,53 +2,47 @@
 
 function calibrate(universe) {
     return universe.map(calibrateIndividual);
+}
 
-    function calibrateIndividual(ind) {
-        return {
-            index: ind.index,
-            name: ind.name,
-            peers: calibratePeers(ind.peers)
-        }
-    }
-
-    function calibratePeers(peers) {
-        if (!peers.length)
-            return peers;
-        peers = calibratePeersSum(peers, mapTrust(peers).reduce((a, b) => a + b, 0));
-        peers = calibratePeersMin(peers, Math.min(...mapTrust(peers)));
-        return calibratePeersMax(peers, Math.max(...mapTrust(peers)));
-    }
-
-    function mapTrust(peers) {
-        return peers.map(p => p.trust);
-    }
-
-    function calibratePeersSum(peers, sumTrust) {
-        if (sumTrust === 1)
-            return peers;
-        const dif = (1.0 - sumTrust) / peers.length;
-        return peers.map(p => Object.assign({}, p, { trust: p.trust + dif }));
-    }
-
-    function calibratePeersMin(peers, minTrust) {
-        return minTrust < -1
-            ? calibratePeersBounds(peers, mid => (mid + 1) / (mid - minTrust))
-            : peers;
-    }
-
-    function calibratePeersMax(peers, maxTrust) {
-        return maxTrust > 1
-            ? calibratePeersBounds(peers, mid => (1 - mid) / (maxTrust - mid))
-            : peers;
-    }
-
-    function calibratePeersBounds(peers, computeScaleFactor) {
-        const mid = 1.0 / peers.length;
-        const scaleFactor = computeScaleFactor(mid);
-        return peers.map(p => Object.assign({}, p, { trust: scale(p.trust) }));
-
-        function scale(trust) {
-            return mid + (trust - mid) * scaleFactor
-        }
+function calibrateIndividual(ind) {
+    return {
+        index: ind.index,
+        name: ind.name,
+        peers: calibratePeers(ind.peers)
     }
 }
+
+function calibratePeers(peers) {
+    if (!peers.length)
+        return peers;
+    const friends = peers.filter(p => p.trust > 0);
+    const foes = peers.filter(p => p.trust < 0);
+    const strangers = peers.filter(p => p.trust === 0);
+    var calibratedPeers = calibrateNonStrangers(friends, 1.0)
+        .concat(calibrateNonStrangers(foes, -1.0))
+        .concat(strangers)
+    calibratedPeers.sort((a, b) => a.index - b.index);
+    return calibratedPeers;
+}
+
+function calibrateNonStrangers(peers, expectedSum) {
+    if (!peers.length)
+        return peers;
+    return calibratePeersSum(peers, sum(mapTrust(peers)), expectedSum);
+}
+
+function sum(arr) {
+    return arr.reduce((a, b) => a + b, 0.0);
+}
+
+function mapTrust(peers) {
+    return peers.map(p => p.trust);
+}
+
+function calibratePeersSum(peers, sumTrust, expectedSum) {
+    if (sumTrust === expectedSum)
+        return peers;
+    return peers.map(p => Object.assign({}, p, { trust: p.trust * expectedSum / sumTrust }));
+}
+
+module.exports = calibrate;
---
+++ App/js/sim/trustCalibrator.test.js
@@ -0,0 +1,137 @@
+"use strict";
+
+let calibrate = require('./trustCalibrator');
+
+test('given empty universe, return copy of universe', () => {
+    const universe = [];
+    expect(calibrate(universe)).toStrictEqual(universe);
+});
+
+test('given universe with one individual without trust, return deep copy of universe', () => {
+    const universe = [{
+        index: 0,
+        name: { first: 'Howard', last: 'Aiken' },
+        peers: []
+    }];
+    const res = calibrate(universe);
+    expect(res).toStrictEqual(universe);
+    expect(res[0]).not.toBe(universe[0]);
+});
+
+test('given universe with two individuals who trust each other, return deep copy of universe with trust = 1', () => {
+    const universe = [
+        {
+            index: 0,
+            name: { first: 'Howard', last: 'Aiken' },
+            peers: [{ index: 1, trust: 0.5 }]
+        },
+        {
+            index: 1,
+            name: { first: 'Ada', last: 'Byron' },
+            peers: [{ index: 0, trust: 3 }]
+        },
+    ];
+    const res = calibrate(universe);
+    expect(res.length).toBe(2);
+    const howard = res[0];
+    const ada = res[1];
+    expect(howard.name.first).toBe('Howard');
+    expect(howard.peers[0].index).toBe(1);
+    expect(howard.peers[0].trust).toBe(1);
+    expect(ada.name.first).toBe('Ada');
+    expect(ada.peers[0].index).toBe(0);
+    expect(ada.peers[0].trust).toBe(1);
+});
+
+test('given universe with two individuals who distrust each other, return deep copy of universe with trust = -1', () => {
+    const universe = [
+        {
+            index: 0,
+            name: { first: 'Howard', last: 'Aiken' },
+            peers: [{ index: 1, trust: -0.5 }]
+        },
+        {
+            index: 1,
+            name: { first: 'Ada', last: 'Byron' },
+            peers: [{ index: 0, trust: -3 }]
+        },
+    ];
+    const res = calibrate(universe);
+    expect(res.length).toBe(2);
+    const howard = res[0];
+    const ada = res[1];
+    expect(howard.name.first).toBe('Howard');
+    expect(howard.peers[0].index).toBe(1);
+    expect(howard.peers[0].trust).toBe(-1);
+    expect(ada.name.first).toBe('Ada');
+    expect(ada.peers[0].index).toBe(0);
+    expect(ada.peers[0].trust).toBe(-1);
+});
+
+test('given universe with two individuals who are indifferent to each other, return deep copy of universe', () => {
+    const universe = [
+        {
+            index: 0,
+            name: { first: 'Howard', last: 'Aiken' },
+            peers: [{ index: 1, trust: 0 }]
+        },
+        {
+            index: 1,
+            name: { first: 'Ada', last: 'Byron' },
+            peers: [{ index: 0, trust: 0 }]
+        },
+    ];
+    const res = calibrate(universe);
+    expect(res).toStrictEqual(universe);
+});
+
+test('given universe with three individuals who trust each other, each individuals sum of trust is 1', () => {
+    const universe = [
+        {
+            index: 0,
+            name: { first: 'Howard', last: 'Aiken' },
+            peers: [{ index: 1, trust: 1 }, { index: 2, trust: 1 }]
+        },
+        {
+            index: 1,
+            name: { first: 'Ada', last: 'Byron' },
+            peers: [{ index: 0, trust: 1 }, { index: 2, trust: 1 }]
+        },
+        {
+            index: 2,
+            name: { first: 'Noam', last: 'Chomsky' },
+            peers: [{ index: 0, trust: 1 }, { index: 1, trust: 1 }]
+        }
+    ];
+    const res = calibrate(universe);
+    expect(res.length).toBe(3);
+    const howard = res[0];
+    const ada = res[1];
+    const noam = res[2];
+    expect(howard.name.first).toBe('Howard');
+    expect(ada.name.first).toBe('Ada');
+    expect(noam.name.first).toBe('Noam');
+    expect(howard.peers[0].trust).toBe(.5);
+    expect(howard.peers[1].trust).toBe(.5);
+    expect(ada.peers[0].trust).toBe(.5);
+    expect(ada.peers[1].trust).toBe(.5);
+    expect(noam.peers[0].trust).toBe(.5);
+    expect(noam.peers[1].trust).toBe(.5);
+});
+
+test('given individual who trust some and distrust some, calibrated individual has sum of positive trust = 1 and sum of negative trust = -1', () => {
+    const universe = [
+        {
+            index: 0,
+            name: { first: 'Howard', last: 'Aiken' },
+            peers: [{ index: 1, trust: .1 }, { index: 2, trust: -.2 }, { index: 3, trust: .3 }, { index: 4, trust: -.4 }]
+        }
+    ];
+    const res = calibrate(universe);
+    expect(res.length).toBe(1);
+    const howard = res[0];
+    expect(howard.peers[0].trust).toBeCloseTo(.25, 5);
+    expect(howard.peers[1].trust).toBeCloseTo(-1 / 3.0, 5);
+    expect(howard.peers[2].trust).toBeCloseTo(.75, 5);
+    expect(howard.peers[3].trust).toBeCloseTo(-2 / 3.0, 5);
+});
