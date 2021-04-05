b9f8e8fdbef222ba748ed927040dfb87246ec61a
Joakim Sigvald <joakim.sigvald@gmail.com>
2021-04-05 22:39:31 +0200

    alternative relationComputer where partial results could be cached and reused

--- App/js/sim/relationComputer.js
+++ App/js/sim/relationComputer.js
@@ -32,6 +32,53 @@ var createRelationComputer = function (util, trustCalibrator) {
         }
     }
 
+    function computeRelations2(universe) {
+        const threshold = 0.00002;
+        const calibratedUniverse = trustCalibrator.calibrate(universe);
+        const n = calibratedUniverse.length;
+        var relations = calibratedUniverse.map(source => computeSourceRelations(1.0, source, [source.index]) || createZeroArray(n));
+        truncateLower(relations, n, 0);
+        return relations;
+
+        function computeSourceRelations(factor, source, ancestorIndices) {
+            if (factor < threshold)
+                return null;
+            const additions = source.peers
+                .filter(peer => !ancestorIndices.includes(peer.index))
+                .map(computePeerRelations);
+            if (!additions.length)
+                return null;
+            if (additions.length === 1)
+                return additions[0];
+            const retVal = additions.shift();
+            additions.forEach(addition => {
+                for (var i = 0; i < n; i++) {
+                    retVal[i] = retVal[i] + addition[i];
+                }
+            });
+            return retVal;
+
+            function computePeerRelations(peer) {
+                const addition = factor * peer.trust;
+                const sourceRelations = computeSourceRelations(
+                    0.5 * addition, calibratedUniverse[peer.index], ancestorIndices.concat([peer.index]))
+                    || createZeroArray(n);
+                sourceRelations[peer.index] = sourceRelations[peer.index] + addition;
+                return sourceRelations;
+            }
+        }
+
+        function createZeroArray(n) {
+            return new Array(n).fill(0);
+        }
+
+        function truncateLower(relations, n, min) {
+            for (var x = 0; x < n; x++)
+                for (var y = 0; y < n; y++)
+                    relations[x][y] = Math.max(min, relations[x][y]);
+        }
+    }
+
     return {
         computeRelations: computeRelations
     }
